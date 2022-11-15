84fe935210f2f3f27fb47134382053ade31c9f28
thekillerregs <thekillerregs@gmail.com>
2022-11-15 10:54:09 -0300

    Specific ItemMeta types such as HelmetItemMeta

--- src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
+++ src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
@@ -1,18 +1,40 @@
 package tk.thekillerregs.tkspigot;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TkSpigot extends JavaPlugin {
+public final class TkSpigot extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage("§bOii!");
-
+    Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e)
+    {
+        ItemStack helmet = new ItemStack(Material.LEATHER_HELMET, 1);
+        LeatherArmorMeta helmetMeta = (LeatherArmorMeta) helmet.getItemMeta();
+        helmetMeta.setColor(Color.AQUA);
+        helmet.setItemMeta(helmetMeta);
+        e.getPlayer().getInventory().addItem(helmet);
+
+
+    }
+
 }
