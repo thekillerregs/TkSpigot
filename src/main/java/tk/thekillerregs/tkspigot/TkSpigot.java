06952c107d756fdb323c349cd5e8d1c09f34dedf
thekillerregs <thekillerregs@gmail.com>
2022-11-18 22:20:03 -0300

    AttributeModifiers

--- src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
+++ src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
@@ -3,6 +3,7 @@ package tk.thekillerregs.tkspigot;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.*;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.block.banner.PatternType;
@@ -23,10 +24,7 @@ import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BannerMeta;
-import org.bukkit.inventory.meta.BookMeta;
-import org.bukkit.inventory.meta.FireworkMeta;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.inventory.meta.*;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -53,6 +51,13 @@ public final class TkSpigot extends JavaPlugin implements Listener {
         if(e.getPlayer().getInventory().getItemInMainHand()==null ^ e.getPlayer().getInventory().getItemInMainHand().getType() == Material.AIR)
         return;
         Player player = e.getPlayer();
+        AttributeModifier modifier = new AttributeModifier("generic.attack_damage", 100, AttributeModifier.Operation.ADD_NUMBER);
+        ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
+        ItemMeta im = item.getItemMeta();
+        im.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
+        item.setItemMeta(im);
+        player.sendMessage("§bSeu item recebeu 100 AD");
+
 
 
     }
