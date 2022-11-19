3ea22a6bd0a97da76d4f42e22aa7d66df131cea0
thekillerregs <thekillerregs@gmail.com>
2022-11-18 22:34:16 -0300

    BlockState and BlockData

--- src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
+++ src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
@@ -5,9 +5,14 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.block.banner.PatternType;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Rail;
+import org.bukkit.block.data.type.Cake;
 import org.bukkit.block.data.type.Fire;
+import org.bukkit.block.data.type.GlassPane;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
@@ -18,10 +23,8 @@ import org.bukkit.entity.*;
 import org.bukkit.FireworkEffect.Type;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerResourcePackStatusEvent;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.*;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.*;
@@ -46,20 +49,21 @@ public final class TkSpigot extends JavaPlugin implements Listener {
 
 
     @EventHandler
-    public void onEvent(PlayerToggleSneakEvent e)
+    public void onEvent(PlayerInteractEvent e)
     {
-        if(e.getPlayer().getInventory().getItemInMainHand()==null ^ e.getPlayer().getInventory().getItemInMainHand().getType() == Material.AIR)
-        return;
-        Player player = e.getPlayer();
-        AttributeModifier modifier = new AttributeModifier("generic.attack_damage", 100, AttributeModifier.Operation.ADD_NUMBER);
-        ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
-        ItemMeta im = item.getItemMeta();
-        im.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
-        item.setItemMeta(im);
-        player.sendMessage("§bSeu item recebeu 100 AD");
-
-
-
+        if(e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            BlockState state = e.getClickedBlock().getState();
+            BlockData data = state.getBlockData();
+            if (data instanceof Cake) {
+                ((Cake) data).setBites(2);
+            } else if (data instanceof GlassPane) {
+                ((GlassPane) data).setWaterlogged(true);
+            } else if (data instanceof Rail) {
+                ((Rail) data).setShape(Rail.Shape.NORTH_WEST);
+            }
+            state.setBlockData(data);
+            state.update();
+        }
     }
 
 
