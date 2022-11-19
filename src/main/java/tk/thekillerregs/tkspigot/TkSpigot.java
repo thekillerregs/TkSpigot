1aa1cdd0a7bb0e61d01ca6025e4352669e63f0f8
thekillerregs <thekillerregs@gmail.com>
2022-11-19 19:44:25 -0300

    Custom Recipes

--- src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
+++ src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
@@ -4,12 +4,15 @@ package tk.thekillerregs.tkspigot;
 import com.google.gson.Gson;
 import org.bukkit.*;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.*;
 
 import org.bukkit.event.server.MapInitializeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.MapMeta;
 import org.bukkit.map.MapView;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,6 +28,19 @@ public final class TkSpigot extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        ItemStack lead = new ItemStack(Material.NETHERITE_SCRAP);
+        ItemMeta im = lead.getItemMeta();
+        im.setDisplayName("§8Lead");
+        lead.setItemMeta(im);
+        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(this, "CUSTOM_LEAD"), lead);
+        recipe.shape(
+                "CIC",
+                "IPI",
+                "CIC");
+        recipe.setIngredient('C', Material.CHAIN);
+        recipe.setIngredient('P', Material.PHANTOM_MEMBRANE);
+        recipe.setIngredient('I', Material.IRON_NUGGET);
+        Bukkit.addRecipe(recipe);
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
@@ -32,27 +48,8 @@ public final class TkSpigot extends JavaPlugin implements Listener {
     @EventHandler
     public void onEvent(PlayerToggleSneakEvent e)
     {
-        ItemStack itemStack = new ItemStack(Material.FILLED_MAP);
-        MapMeta meta = (MapMeta) itemStack.getItemMeta();
-        MapView view = Bukkit.createMap(e.getPlayer().getWorld());
-        view.getRenderers().forEach(m -> {
-            view.removeRenderer(m);
-        });
-        view.addRenderer(new TkRenderer());
-       meta.setMapView(view);
-       itemStack.setItemMeta(meta);
-       e.getPlayer().getInventory().addItem(itemStack);
 
-
-
-
-
-
-
-
-
-
-        }
+    }
 
 
 
