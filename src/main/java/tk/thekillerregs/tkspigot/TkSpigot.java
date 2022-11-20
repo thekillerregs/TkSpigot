899dca6fdbea7876876a01065a5271dcf813b850
thekillerregs <thekillerregs@gmail.com>
2022-11-19 21:15:37 -0300

    Permissions and PermissionAttachments

--- src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
+++ src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
@@ -10,46 +10,71 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.*;
 
+import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public final class TkSpigot extends JavaPlugin implements Listener {
-
-
+    PermissionAttachment attachment;
+private HashMap<UUID, PermissionAttachment> perms = new HashMap<>();
 
     @Override
     public void onEnable(){
     Bukkit.getPluginManager().registerEvents(this, this);
 
+
+
+
     }
 
 
     @EventHandler
-    public void onEvent(PlayerToggleSneakEvent e)
+    public void onEvent(BlockPlaceEvent e)
     {
-    if(e.isSneaking())
-    {
-        String[] lines = new String[]{"§bTk", "§5Tk2", "§dTk3"};
-
-        Location location = e.getPlayer().getLocation();
-
-        for(String line : lines){
-
-
-        ArmorStand stand = (ArmorStand) e.getPlayer().getWorld().spawnEntity(location.subtract(0,0.5,0), EntityType.ARMOR_STAND);
-        stand.setInvisible(true);
-        stand.setGravity(false);
-        stand.setInvulnerable(true);
-        stand.setCustomNameVisible(true);
-        stand.setCustomName(line);
+        Player player = e.getPlayer();
+        if(!player.hasPermission("tk.blocks")){
+            player.sendMessage("§cVocê não pode colocar blocos!!!!!");
+            e.setCancelled(true);
         }
 
     }
+
+    @EventHandler
+    public void onSneak(PlayerToggleSneakEvent e )
+    {
+        Player player = e.getPlayer();
+        if(e.isSneaking())
+        {
+            if(!perms.containsKey(player.getUniqueId()))
+            {
+                attachment = player.addAttachment(this);
+                perms.put(player.getUniqueId(), attachment);
+            }
+            else{
+                attachment = perms.get(player.getUniqueId());
+            }
+
+            if(player.hasPermission("tk.blocks"))
+            {
+                attachment.unsetPermission("tk.blocks");
+                player.sendMessage("§cVocê perdeu a permissão de colocar blocos!");
+            }
+            else{
+                attachment.setPermission("tk.blocks", true);
+                player.sendMessage("§aVocê recebeu a permissão de colocar blocos!");
+            }
+
+
+
+
+        }
+
 
     }
 
