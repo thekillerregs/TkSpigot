ceb41e3a6dbb6acbaf3cddcb538ff5565ed0c358
thekillerregs <thekillerregs@gmail.com>
2022-11-28 14:03:03 -0300

    Putting and retrieving data with MongoDB

--- src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
+++ src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
@@ -1,57 +1,64 @@
 package tk.thekillerregs.tkspigot;
 
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 import java.sql.SQLException;
+import java.util.UUID;
 
 public final class TkSpigot extends JavaPlugin implements Listener {
 
-
-    private Database db;
-    private PlayerManager playerManager;
+    private MongoClient client;
+    private MongoDatabase db;
+    private MongoCollection players;
 
     @Override
     public void onEnable(){
-    db = new Database();
-        try {
-            db.connect();
+    client = new MongoClient();
+    db=client.getDatabase("tk_spigot");
+    players = db.getCollection("players");
+
+    Document document = new Document();
+    document.put("uuid", UUID.randomUUID());
+    document.put("rank", "Owner");
+    document.put("coins", 5);
+    players.insertOne(document);
 
 
+    try(MongoCursor cursor = players.find(Filters.eq("rank", "Owner")).cursor()){
+    while(cursor.hasNext())
+    {
+        Document doc1 = (Document) cursor.next();
+        System.out.println(document.get("coins"));
+    }
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(db.isConnected());
-        playerManager = new PlayerManager();
-
-        Bukkit.getPluginManager().registerEvents(new ConnectionListener(this), this);
+    }
 
     }
 
 
     @Override
     public void onDisable() {
-        db.disconnect();
-
         // Plugin shutdown logic
     }
 
 
 
 
-    public Database getDb() {
-        return db;
-    }
 
 
-    public PlayerManager getPlayerManager() {
-        return playerManager;
-    }
 
 
 }
