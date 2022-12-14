ee2be3b882ae400ec229764ab82cd86a23e439b4
thekillerregs <thekillerregs@gmail.com>
2022-12-14 11:24:53 -0300

    Mojang Mapped NMS setup

--- src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
+++ src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
@@ -1,18 +1,11 @@
 package tk.thekillerregs.tkspigot;
 
 
-
-import com.google.common.io.ByteArrayDataInput;
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
+import net.minecraft.server.level.ServerPlayer;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.messaging.PluginMessageListener;
-import org.enginehub.squirrelid.Profile;
-import org.enginehub.squirrelid.resolver.HttpRepositoryService;
-import org.enginehub.squirrelid.resolver.ProfileService;
-import org.jetbrains.annotations.NotNull;
+
 
 import java.io.IOException;
 
@@ -22,17 +15,8 @@ public final class TkSpigot extends JavaPlugin {
 
     @Override
     public void onEnable(){
-        ProfileService resolver = HttpRepositoryService.forMinecraft();
-        //:trolljogos:
-        try {
-            Profile profile = resolver.findByName("thekillerregs");
-            if(profile!=null) System.out.println(profile.getUniqueId());
-            else System.out.println("§cProfile is null");
-
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
+            //Intentional error
+            ServerPlayer sp = ((CraftPlayer) Bukkit.getPlayer("thekillerregs")).getHandle();
 
     }
 
