580bd53889576b3a1c27270081111866bf959a6c
thekillerregs <thekillerregs@gmail.com>
2022-12-14 22:31:41 -0300

    Clicking on NPCs

--- src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
+++ src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
@@ -1,31 +1,24 @@
 package tk.thekillerregs.tkspigot;
 
 
-import org.bukkit.Particle;
+import io.netty.channel.*;
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import tk.thekillerregs.tkspigot.instance.Cosmetic;
-import tk.thekillerregs.tkspigot.instance.trails.TrailsCommand;
-import tk.thekillerregs.tkspigot.navigation.CosmeticListener;
-import tk.thekillerregs.tkspigot.navigation.CosmeticsCommand;
-import tk.thekillerregs.tkspigot.instance.hats.HatsCommand;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.lang.reflect.Field;
 
 
 public final class TkSpigot extends JavaPlugin {
 
-    private HashMap<UUID, List<Cosmetic>> activeCosmetics = new HashMap<>();
-
+    int npcId;
 
     @Override
     public void onEnable() {
-        getCommand("cosmetics").setExecutor(new CosmeticsCommand(this));
-        getCommand("hats").setExecutor(new HatsCommand(this));
-        getCommand("trails").setExecutor(new TrailsCommand(this));
-        getServer().getPluginManager().registerEvents(new CosmeticListener(this), this);
-        
+        getCommand("npc").setExecutor(new NPCCommand(this));
+        Bukkit.getPluginManager().registerEvents(new ConnectionListener(this), this);
     }
 
 
@@ -34,9 +27,63 @@ public final class TkSpigot extends JavaPlugin {
         // Plugin shutdown logic
     }
 
+    public void inject(Player player) {
+        //Preciso estudar essa bomba
+        ChannelDuplexHandler channelHandler = new ChannelDuplexHandler() {
 
-    public HashMap<UUID, List<Cosmetic>> getActiveCosmetics() {
-        return activeCosmetics;
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object packet) throws Exception {
+                if (packet instanceof ServerboundInteractPacket) {
+                    ServerboundInteractPacket sipk = (ServerboundInteractPacket) packet;
+
+                    Field type = sipk.getClass().getDeclaredField("b");
+                    type.setAccessible(true);
+                    Object data = type.get(sipk);
+                    //Regex é certamente uma das coisas ja criadas
+                    if (data.toString().split("\\$")[1].charAt(0) == 'e') return;
+
+                    try {
+                        Field hand = data.getClass().getDeclaredField("a");
+                        hand.setAccessible(true);
+                        if (!hand.get(data).equals("MAIN_HAND")) return;
+
+
+                    } catch (NoSuchFieldException e) {
+
+                    }
+                    Field id = sipk.getClass().getDeclaredField("a");
+                    id.setAccessible(true);
+                    int entityId = id.getInt(sipk);
+
+                    if(entityId == getNpcId())
+                    {
+                        System.out.println("§aVocê clicou em um npc!");
+                    }
+
+
+                }
+                super.channelRead(ctx, packet);
+            }
+        };
+
+        ChannelPipeline pipeline = ((CraftPlayer) player).getHandle().connection.getConnection().channel.pipeline();
+        pipeline.addBefore("packet_handler", player.getName(), channelHandler);
+    }
+
+    public void stop(Player player) {
+        Channel channel = ((CraftPlayer) player).getHandle().connection.getConnection().channel;
+        channel.eventLoop().submit(() -> {
+            channel.pipeline().remove(player.getName());
+            return null;
+        });
+    }
+
+    public int getNpcId() {
+        return npcId;
+    }
+
+    public void setNpcId(int npcId) {
+        this.npcId = npcId;
     }
 }
 
