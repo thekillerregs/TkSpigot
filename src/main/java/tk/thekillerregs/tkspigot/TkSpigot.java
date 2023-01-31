88259b94b9b2b821307bf46372e45f1a31a97402
thekillerregs <thekillerregs@gmail.com>
2023-01-31 12:14:42 -0300

    the redis incident

--- src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
+++ src/main/java/tk/thekillerregs/tkspigot/TkSpigot.java
@@ -1,17 +1,14 @@
 package tk.thekillerregs.tkspigot;
 
 
-import net.minecraft.network.protocol.game.ClientboundAnimatePacket;
-import net.minecraft.network.protocol.game.ClientboundGameEventPacket;
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
 
-import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public final class TkSpigot extends JavaPlugin implements Listener {
@@ -19,19 +16,41 @@ public final class TkSpigot extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
+        redis();
     }
 
+    private void redis()
+    {
+        Jedis jedis = new Jedis("localhost", 6379, 5000);
+
+
+        Pipeline pipeline = jedis.pipelined();
+        pipeline.set("tk", "thekillerregs");
+        pipeline.expire("tk", 30);
+        pipeline.sync();
+        System.out.println(jedis.get("tk"));
+
+        Map<String, String> fullNames = new HashMap<>();
+        fullNames.put("Tk", "thekillerregs");
+        jedis.hmset("full_names", fullNames);
+        System.out.println(jedis.hgetAll("full_names"));
+
+
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            System.out.println(jedis.get("tk"));
+
+        }, 31*20);
+
+
+
+    }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
 
-    @EventHandler
-    public void onSneak(PlayerDeathEvent e) {
-        ((CraftPlayer) e.getEntity()).getHandle().connection.send(new ClientboundGameEventPacket(ClientboundGameEventPacket.IMMEDIATE_RESPAWN, 1));
-    }
+
 
 
 }
